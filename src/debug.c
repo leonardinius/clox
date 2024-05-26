7d4cb8f8fa3dc76bd591f9dd90a9c2b952482b3e
Leo <leonidms@gmail.com>
2024-05-26 18:40:11 +0300

    add constants skeleton

--- src/debug.c
+++ src/debug.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 void dissassembleChunk(const Chunk *chunk, const char *name)
 {
     printf("== %s ==\n", name);
 
-    for (int offset = 0; offset < chunk->count; offset++)
+    for (int offset = 0; offset < chunk->count;)
     {
         offset = dissassembleInstruction(chunk, offset);
     }
@@ -19,6 +20,10 @@ int dissassembleInstruction(const Chunk *chunk, int offset)
     uint8_t instruction = chunk->code[offset];
     switch (instruction)
     {
+    case OP_CONSTANT:
+        return constantInstruction("OP_CONSTANT", chunk, offset);
+        break;
+
     case OP_RETURN:
         return simpleInstruction("OP_RETURN", offset);
         break;
@@ -28,6 +33,15 @@ int dissassembleInstruction(const Chunk *chunk, int offset)
         return offset + 1;
         break;
     }
+}
+
+int constantInstruction(const char *name, const Chunk *chunk, int offset)
+{
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
 }
 
 int simpleInstruction(const char *name, int offset)
