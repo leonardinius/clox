b99e7baf7a09995b56ae38ed134c287d0c679338
Leo <leonidms@gmail.com>
2024-05-28 00:11:55 +0300

    compiler skeleton

--- src/compiler.c
+++ src/compiler.c
@@ -1,22 +1,98 @@
 #include "compiler.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "common.h"
 #include "scanner.h"
+#include "vm.h"
 
-void compile(const char* source) {
-    initScanner(source);
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%04d ", token.line);
-            line = token.line;
-        } else {
-            printf("  |  ");
-        }
-        printf("%-19s '%.*s'\n", getTokenTypeName(token.type), token.length,
-               token.start);
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
 
-        if (token.type == TOKEN_EOF) break;
+Parser parser;
+Chunk* compilingChunk;
+Chunk* currentChunk() { return compilingChunk; }
+
+static void initParser(Parser* parser) {
+    Token t;
+    t.start = NULL;
+    parser->previous = parser->current = t;
+
+    parser->hadError = false;
+    parser->panicMode = false;
+}
+
+static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // Nothing.
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+// static void error(const char* message) { errorAt(&parser.previous, message);
+// }
+
+static void advance() {
+    parser.previous = parser.current;
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(TokenType tokenType, const char* message) {
+    if (parser.current.type == tokenType) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+// static void emitBytes(uint8_t byte1, uint8_t byte2) {
+//     writeChunk(currentChunk(), byte1, parser.previous.line);
+//     writeChunk(currentChunk(), byte2, parser.previous.line);
+// }
+
+static void expression() {}
+
+static void emitReturn() { emitByte(OP_RETURN); }
+static void endCompiler() { emitReturn(); }
+
+bool compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+    compilingChunk = chunk;
+    initParser(&parser);
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+
+    return !parser.hadError;
 }
